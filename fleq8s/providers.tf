6ab31c80c03114bc405c54e89c4a5360081e2f51
adambaksa <151851619+adambaksa@users.noreply.github.com>
2024-03-20 09:41:46 +0100

    Create providers.tf

---
+++ fleq8s/providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  subscription_id = "e9f2368c-0dff-43f0-8a05-f7d8ffe8df6e"
+  client_id       = "5ccb6a14-ebc5-4fb2-aa19-7d9f2dae0374"
+  client_secret   = "yuN8Q~Ky46vbHH1.Em9gIRrEiggnG2rKbNDOna-w"
+  tenant_id       = "7e251758-ea33-468d-9d61-4a8e5ebea98d"
+  features {}
+}
