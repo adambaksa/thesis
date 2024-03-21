7374fc3f0350cfe50514e2535599dec272737ead
adambaksa <151851619+adambaksa@users.noreply.github.com>
2024-03-21 11:29:37 +0100

    Update variables.tf

--- fleq8s/variables.tf
+++ fleq8s/variables.tf
@@ -1,21 +1,41 @@
 variable "resource_group_name" {
-  description = "app-grp"
+  description = "The name of the resource group where all resources will be created."
+  type        = string
+  default     = "app_grp"
 }
 
 variable "location" {
-  description = "North Europe"
+  description = "The Azure region where the resource group and all resources will be created."
+  type        = string
   default     = "North Europe"
 }
 
-variable "app_vm" {
-  description = "neumann_vm01"
+variable "vm_name" {
+  description = "The base name of the virtual machine that will be used to generate related resource names, such as subnets."
+  type        = string
+  default     = "neumann_vm01"
 }
 
 variable "admin_username" {
-  description = "fleq8s"
+  description = "The administrator username for the virtual machine."
+  type        = string
+  default     = "fleq8s"
 }
 
 variable "admin_password" {
-  description = "Azure@123"
+  description = "The administrator password for the virtual machine."
+  type        = string
   sensitive   = true
+  default     = "Azure@123"
+}
+
+variable "virtual_network_name" {
+  description = "The name of the virtual network to be created."
+  type        = string
+  default     = "vnet"
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network."
+  type        = list(string)
 }
