1180de069e1fa8a80ea5f36f74423b091b950ada
adambaksa <151851619+adambaksa@users.noreply.github.com>
2024-03-21 12:01:35 +0100

    Update variables.tf

--- fleq8s/variables.tf
+++ fleq8s/variables.tf
@@ -38,4 +38,5 @@ variable "virtual_network_name" {
 variable "address_space" {
   description = "The address space for the virtual network."
   type        = list(string)
+  default     = ["10.0.0.0/16"]
 }
