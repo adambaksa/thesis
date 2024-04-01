ea798a3e11cc34bf1f67fed53f7a1914c9c41f62
adambaksa <151851619+adambaksa@users.noreply.github.com>
2024-04-01 11:16:00 +0200

    Delete fleq8s directory

--- fleq8s/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  subscription_id = "bf9b83d8-67d1-4691-9cb8-3de759b0fa86"
-  client_id       = "383dcc0f-3f11-44c1-8b29-032f7f25ad7a"
-  client_secret   = "~ec8Q~Er1LCv_8nKZZDqmS-tFV~-bi.3x_r.rbq7"
-  tenant_id       = "e70d904c-182f-4d56-9452-4421819e0ff3"
-  features {}
-}
--- fleq8s/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-resource_group_name = "app_grp"
-location            = "North Europe"
-vm_name             = "neumann_vm01"
-admin_username      = "fleq8s"
-admin_password      = "Azure@123"
-virtual_network_name = "vnet"
